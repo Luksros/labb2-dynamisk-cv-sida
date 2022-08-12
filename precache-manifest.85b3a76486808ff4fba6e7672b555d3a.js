959c7021e76a1201ea152b4f69e6c69fa98d0f8a
Luksros <lukasrose@live.se>
2022-08-12 16:15:27 +0200

    Updates

--- precache-manifest.85b3a76486808ff4fba6e7672b555d3a.js
+++ precache-manifest.85b3a76486808ff4fba6e7672b555d3a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "738e29614fb50a0c01dc",
+    "revision": "5298ca1640ad5be2ce8d",
     "url": "/labb2-dynamisk-cv-sida/static/css/main.8ea8ae60.chunk.css"
   },
   {
-    "revision": "738e29614fb50a0c01dc",
-    "url": "/labb2-dynamisk-cv-sida/static/js/main.738e2961.chunk.js"
+    "revision": "5298ca1640ad5be2ce8d",
+    "url": "/labb2-dynamisk-cv-sida/static/js/main.5298ca16.chunk.js"
   },
   {
     "revision": "fe8104d9b7ea19c8e84c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/labb2-dynamisk-cv-sida/static/js/runtime~main.854605fb.js"
   },
   {
-    "revision": "cf936a590958df9b32ed5c2d0cce9580",
+    "revision": "4fd5516bfa9931d83b755c2db48fbb06",
     "url": "/labb2-dynamisk-cv-sida/index.html"
   }
 ];
