392801de2540eaa8059d33021e206668c73ff315
Luksros <lukasrose@live.se>
2022-08-23 10:26:03 +0200

    cv pic url fix

--- src/reactPages/Cv.js
+++ src/reactPages/Cv.js
@@ -49,7 +49,7 @@ function Cv(){
         <div className="cvBox">
             <CvScript></CvScript>
             <div className = "pictureContainer">   
-                <img src="https://previews.dropbox.com/p/thumb/ABl68r37kLMNFeawoDArlN9un0dwSdv9yGsLiVAUbCI9iS0b8c2kFh4-JDGeXgEIIg3iw-KhrzAu7Q2nE0TtV-jQLbU37ybTVBfxreHUa8Ecv3U5qYXfB69yCQp8i0T-lkriTL5_G5fZorhijQPqaUWjA8V0lSO0vL0Sgv7bmfgNaaEjHLV-HGYTcEBWoGo7usyXOMixbY5dnB57zwTtmBrLtkV1t9TxGnDC3jWx1Cfo_9jtUkqhE-O-6Z3-SLb9KeobillemKgHlK8mbV8g385Mhxjo305bu4K6pgeMYl8MmQe2KY0NGGFKkpSV8rIM5p2-LFBjZAn3u9Q6_tkrHQBo4T4Jhs6fQIDBXR1y1uMgglj_WVj779AfVmF7MMi_EAM/p.jpeg" alt="Porträttfoto av Lukas Rose"/>
+                <img src="https://previews.dropbox.com/p/thumb/ABoes4wOgAbUp50D7cgAwPv6aSf8xCNk3UBth4XT29C1thytZSRaiZMbUwuEkzwGR9fGL5RQ-r23NJri2y0uOMAyMzV_iDrgydWAEbfuu8po7gRSvc5P5-FBAi0AJVcaRWNfTSAwnUR1zFyM4fVGxrT8zq_825tkX5l-UUWIo6r7RrZVB31mtz7w2SYN2cK1avy2GqCe7K5fVl9nppNlohwCTRtyo-aeqttBx49d8m0Nh5yZQ-4p2H8i3fTpDXMib5Yz7fq56Crw3e3NEcS9XJkyWsXfJpG0Op49Vsj6ve6KEKjfFr8mcG_09KazC1GqvlBk536AVQ_uJhtBuaTnyrlEg9bzRrlZrzOQVyk-8Ug9nehtxONvZg7N4_lRgNPDqk4QuzjRtlgb2KLhkVE4HL4i/p.jpeg" alt="Porträttfoto av Lukas Rose"/>
             </div>      
         </div>
         <br/>
